f00c0a743f79d39dc4fc4337d66a65688f119ecf
C836 <gl.sepiol@gmail.com>
2022-09-23 21:27:53 -0300

    feat: importando fontes e cores principais da página

---
+++ src/global.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  yellow: "#FFDD00",
+  black: "#000000",
+  gray: "#D9D9D9",
+  white: "#ffffff",
+
+  chart: {
+    red: "#E56E25",
+    orange: "#F18A1F",
+    blue: "#3BA3D2",
+    green: "#4EB966",
+    yellow: "#EFB119",
+  },
+};
