b78984f56b246f781e9d36525a502f889fd47046
C836 <gl.sepiol@gmail.com>
2022-09-25 13:42:45 -0300

    feat: criando conexão com o banco de dados
    adicionando arquivo .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
